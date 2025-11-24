c93abdcd5bed90882df5f53c0b8cbf395034c50a
siddhant-abi <siddhant.solanki@ab-inbev.com>
2025-11-24 21:14:33 +0530

    no edge

--- expo_v2/supabase.js
+++ expo_v2/supabase.js
@@ -1,11 +1,16 @@
 export async function loadConfig() {
-    const res = await fetch("config.json");
-    const config = await res.json();
-    window.APP_CONFIG = config;
+  const res = await fetch("config.json");
+  const config = await res.json();
+  window.APP_CONFIG = config;
 
-    window.supabase = window.supabase ||
-        (await import("https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"))
-            .createClient(config.SUPABASE_URL, config.SUPABASE_ANON_KEY);
+  const { createClient } = await import(
+    "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
+  );
 
-    return config;
+  window.supabase = createClient(
+    config.SUPABASE_URL,
+    config.SUPABASE_ANON_KEY
+  );
+
+  return config;
 }
